b22708862bc40ac6087184be6687d35dfbb01af8
danielyanes22 <daniel.yanes@nottingham.ac.uk>
2025-04-15 15:46:51 +0100

    media components plot -> gridlines removed, NaN3 formatted and y axis labels increased

--- data_viz/media_components.R
+++ data_viz/media_components.R
@@ -1,7 +1,18 @@
 library(tidyverse)
+library(stringr)
 
 # Load data
 media_components <- read.csv("data/unprocessed/media_components.csv")
+
+# Capitalise all component names
+media_components$component_name <- str_to_title(media_components$component_name)
+
+# Fix NaN3 to NaN[3] for subscript
+media_components$component_name <- ifelse(
+  media_components$component_name == "Nan3",
+  "NaN[3]",
+  media_components$component_name
+)
 
 # Calculate % of database and reorder
 component_freq <- media_components %>%
@@ -11,14 +22,16 @@ component_freq <- media_components %>%
     Media_component = fct_reorder(component_name, Percent, .desc = TRUE)  # Reorder factor
   )
 
+# Custom labeller to parse only NaN[3], keep others as regular strings
+custom_labeller <- function(x) {
+  ifelse(x == "NaN[3]", expression(NaN[3]), x)
+}
 
-component_freq <- component_freq %>%
-  rename(Media_component = component_name)
-
+# Plot
 plot_media_components <- ggplot(component_freq, aes(x = Media_component, y = Percent, fill = Media_component)) +
   geom_col(color = "black", width = 0.7) +
   geom_text(aes(
-    label = paste0(round(Percent, 1)), 
+    label = paste0(round(Percent, 1)),
     color = "black"
   ),
   vjust = -0.3,
@@ -31,6 +44,7 @@ plot_media_components <- ggplot(component_freq, aes(x = Media_component, y = Per
     labels = scales::label_number(scale = 1),
     expand = expansion(mult = c(0, 0.05))
   ) +
+  scale_x_discrete(labels = custom_labeller) +
   labs(
     x = "Media component",
     y = "% of database"
@@ -40,10 +54,13 @@ plot_media_components <- ggplot(component_freq, aes(x = Media_component, y = Per
     legend.position = "none",
     axis.text.x = element_text(angle = 60, hjust = 1, vjust = 0.9, face = "bold"),
     axis.text.y = element_text(face = "bold"),
-    axis.title.x = element_text(face = "bold"),
-    axis.title.y = element_text(face = "bold")
+    axis.title.x = element_text(face = "bold", size = 14),
+    axis.title.y = element_text(face = "bold", size = 14),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
   )
 
+# Print plot
 plot_media_components
 
 
